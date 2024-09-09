e8d4103ae135f1fb365b874aa0661a5be96225d2
xcdnlgd <xcdnlgd@hotmail.com>
2024-09-09 23:41:06 +0800

    add markdown support

--- lua/community.lua
+++ lua/community.lua
@@ -11,5 +11,6 @@ return {
   { import = "astrocommunity.pack.sql" },
   { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
   -- import/override with your plugins folder
 }
