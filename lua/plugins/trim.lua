3c9a1930702a587e76682a9a593585a043e51ec3
xcdnlgd <xcdnlgd@hotmail.com>
2024-10-19 20:13:10 +0800

    disable trim.nvim in neo-tree

--- lua/plugins/trim.lua
+++ lua/plugins/trim.lua
@@ -4,7 +4,7 @@ return {
   opts = {
     -- if you want to ignore markdown file.
     -- you can specify filetypes.
-    ft_blocklist = { "TelescopePrompt", "TelescopeResults", "toggleterm", "lazy", "alpha" },
+    ft_blocklist = { "TelescopePrompt", "TelescopeResults", "toggleterm", "lazy", "alpha", "neo-tree" },
 
     -- if you want to remove multiple blank lines
     -- patterns = {
