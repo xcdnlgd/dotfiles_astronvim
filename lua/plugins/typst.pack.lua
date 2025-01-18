b9d0b0a01ef1c4b02e4676e146a7d229cc81e290
xcdnlgd <xcdnlgd@hotmail.com>
2025-01-18 22:34:57 +0800

    tinymist for typst

--- lua/plugins/typst.pack.lua
+++ lua/plugins/typst.pack.lua
@@ -11,14 +11,14 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "typst_lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "tinymist" })
     end,
   },
   {
     "WhoIsSethDaniel/mason-tool-installer.nvim",
     optional = true,
     opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "typst-lsp" })
+      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "tinymist" })
     end,
   },
   {
