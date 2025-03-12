3c5958ebf9e75be58b413725b607280b264e904e
xcdnlgd <xcdnlgd@hotmail.com>
2025-03-12 16:49:28 +0800

    toggle term float highlight to normal to fix cursor line invisible

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,12 +1,13 @@
 ---@type LazySpec
 return {
   "akinsho/toggleterm.nvim",
-  opts = function (_, opts)
-    opts.on_open = function (_)
-      vim.wo.spell = false
-    end
-    opts.on_close = function(_)
-      vim.wo.spell = true
-    end
-  end
+  opts = function(_, opts)
+    opts.on_open = function(_) vim.wo.spell = false end
+    opts.on_close = function(_) vim.wo.spell = true end
+    opts.highlights = {
+      NormalFloat = {
+        link = "Normal",
+      },
+    }
+  end,
 }
